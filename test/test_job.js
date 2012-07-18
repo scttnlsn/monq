ad0d03b2c64144ac8185d1d60712d64c13619de3
Scott Nelson <scottbnel@gmail.com>
2012-07-18 13:16:04 -0400

    Added basic job manipulation

--- test/test_job.js
+++ test/test_job.js
@@ -5,12 +5,17 @@ var Job = require('../lib/job');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/monq_tests');
 
 describe('Job', function() {
+    beforeEach(function(done) {
+        Job.remove(done);
+    });
+
     describe('when enqueueing', function() {
         var job;
 
         beforeEach(function(done) {
             Job.enqueue('foo', { bar: 'baz' }, 'qux', function(err, instance) {
                 if (err) return done(err);
+
                 job = instance;
                 done();
             });
@@ -29,11 +34,92 @@ describe('Job', function() {
         });
 
         it('has an enqueued date', function() {
+            assert.ok(job.enqueued);
             assert.ok(job.enqueued < Date.now());
         });
 
         it('has `queued` status', function() {
             assert.equal(job.status, 'queued');
+        });
+    });
+
+    describe('when dequeueing', function() {
+        var job;
+
+        beforeEach(function(done) {
+            Job.enqueue('foo1', { bar: 'baz' }, 'qux', function(err) {
+                if (err) return done(err);
+
+                Job.enqueue('foo2', { bar: 'baz' }, 'qux', function(err) {
+                    if (err) return done(err);
+
+                    Job.dequeue('qux', function(err, instance) {
+                        if (err) return done(err);
+
+                        job = instance;
+                        done();
+                    });
+                });
+            });
+        });
+
+        it('finds first job for given queue', function() {
+            assert.ok(job);
+            assert.equal(job.name, 'foo1');
+            assert.equal(job.queue, 'qux');
+        });
+
+        it('has a dequeued date', function() {
+            assert.ok(job.dequeued);
+            assert.ok(job.dequeued < Date.now());
+        });
+
+        it('has `dequeued` status', function() {
+            assert.equal(job.status, 'dequeued');
+        });
+    });
+
+    describe('when completing job', function() {
+        var job;
+
+        beforeEach(function(done) {
+            job = new Job({ name: 'foo', queue: 'qux' });
+            job.complete({ bar: 'baz' }, done);
+        });
+
+        it('has a result', function() {
+            assert.deepEqual(job.result, { bar: 'baz' });
+        });
+
+        it('has an ended time', function() {
+            assert.ok(job.ended);
+            assert.ok(job.ended < Date.now());
+        });
+
+        it('has a `complete` status', function() {
+            assert.equal(job.status, 'complete');
+        });
+    });
+
+    describe('when failing job', function() {
+        var job;
+
+        beforeEach(function(done) {
+            job = new Job({ name: 'foo', queue: 'qux' });
+            job.fail(new Error('bar baz'), done);
+        });
+
+        it('has an error', function() {
+            assert.equal(job.error, 'bar baz');
+        });
+
+        it('has an ended time', function() {
+            assert.ok(job.ended);
+            assert.ok(job.ended < Date.now());
+        });
+
+        it('has a `failed` status', function() {
+            assert.equal(job.status, 'failed');
         });
     });
 });
