cdb4b6cd9c1a2682ca9d016101f23d8d1c0c831c
Scott Nelson <scottbnel@gmail.com>
2012-07-19 16:11:05 -0400

    Removing user and session (not really suitable for monq core)

--- test/helpers.js
+++ test/helpers.js
@@ -1,12 +1,5 @@
 var mongoose = require('mongoose');
-var mubsub = require('mubsub');
 
 exports.connect = function() {
-    var db = process.env.MONGODB_URI || 'mongodb://localhost:27017/monq_tests';
-    
-    mongoose.connect(db);
-
-    if (mubsub.connection.readyState === 0) {
-        mubsub.connect(db);
-    }
+    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/monq_tests');
 };
