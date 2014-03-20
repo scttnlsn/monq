d8a91a8740ddc611858efc0f659033735a486791
Chris Scribner <scriby@gmail.com>
2014-03-20 16:07:09 -0400

    Add test for minPriority worker

--- test/test_priority.js
+++ test/test_priority.js
@@ -89,3 +89,71 @@ describe('job', function() {
         });
     });
 });
+
+describe('worker', function() {
+    var queue, handler, worker;
+
+    before(function(done) {
+        queue = new Queue({ db: helpers.db });
+
+        handler = sinon.spy(
+            function(params, callback){
+                callback();
+            }
+        );
+
+        worker = new Worker([ queue ], { interval: 10, minPriority: 1 });
+        worker.register({ priority: handler });
+
+        queue.collection.remove({}, done);
+    });
+
+    after(function(done){
+        worker.stop(done);
+    });
+
+    after(function(done){
+        queue.collection.remove({}, done);
+    });
+
+    describe('with minimum priority', function(){
+        it('enqueues a negative 1 priority job', function(done){
+            queue.enqueue('priority', { data: 'negative-priority' }, { priority: -1 }, done);
+        });
+
+        it('enqueues a default priority job', function(done){
+            queue.enqueue('priority', { data: 'priority-default' }, done);
+        });
+
+        it('enqueues a priority 0 job', function(done){
+            queue.enqueue('priority', { data: 'priority-0' }, { priority: 0 }, done);
+        });
+
+        it('enqueues a priority 1 job', function(done){
+            queue.enqueue('priority', { data: 'priority-1' }, { priority: 1 }, done);
+        });
+
+        it('enqueues a priority 2 job', function(done){
+            queue.enqueue('priority', { data: 'priority-2' }, { priority: 2 }, done);
+        });
+
+        it('starts working', function(){
+            worker.start();
+        });
+
+        it('calls the handler 2 times', function(done){
+            (function hasFinished(){
+                if(handler.calledTwice){
+                    done();
+                } else {
+                    setTimeout(hasFinished, 10);
+                }
+            })();
+        });
+
+        it('processes the jobs in the correct order', function(){
+            assert.equal(handler.args[0][0].data, 'priority-2');
+            assert.equal(handler.args[1][0].data, 'priority-1');
+        });
+    });
+});
